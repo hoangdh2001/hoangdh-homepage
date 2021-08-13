f27cc9d38b3577c6019e800423a463459a939d5c
Lucas Do <dohuyhoang.se.dev@gmail.com>
2021-08-13 14:30:00 +0900

    chore: add Tailwind CSS and Prism theme

---
+++ tailwind.config.js
@@ -0,0 +1,99 @@
+// @ts-check
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+/** @type {import("tailwindcss/types").Config } */
+module.exports = {
+  content: [
+    './node_modules/pliny/**/*.js',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './layouts/**/*.{js,ts,tsx}',
+    './data/**/*.mdx',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      },
+      fontFamily: {
+        sans: ['var(--font-ibm-plex-mono)', ...fontFamily.mono],
+        mono: ['var(--font-ibm-plex-mono)', ...fontFamily.mono],
+        serif: ['var(--font-fraunces)', ...fontFamily.serif],
+      },
+      colors: {
+        primary: {
+          50:  '#f0fdf0',
+          100: '#d8fad8',
+          200: '#bcf5bc',
+          300: '#a3f0a0',
+          400: '#7ce87a',
+          500: '#56d954',
+          600: '#3a9e38',
+          700: '#2a7828',
+          800: '#1a5218',
+          900: '#0e3010',
+          950: '#071808',
+        },
+        gray: colors.neutral,
+      },
+      zIndex: {
+        60: '60',
+        70: '70',
+        80: '80',
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            code: {
+              color: theme('colors.indigo.500'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+      keyframes: {
+        shine: {
+          '0%': { 'background-position': '100%' },
+          '100%': { 'background-position': '-100%' },
+        },
+      },
+      animation: {
+        shine: 'shine 5s linear infinite',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
