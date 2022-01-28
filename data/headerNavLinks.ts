89efd48c2b2c7b83d58ecd4edb3bc3c36e183605
Lucas Do <dohuyhoang.se.dev@gmail.com>
2022-01-28 14:00:00 +0900

    refactor: extract nav links

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/notes', title: 'Notes' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
