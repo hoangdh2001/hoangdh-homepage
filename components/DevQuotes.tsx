06c4e517d3a5ebd0a3324d26694dbd866f82008e
Lucas Do <dohuyhoang.se.dev@gmail.com>
2022-05-30 14:00:00 +0900

    feat: add developer quotes widget

---
+++ components/DevQuotes.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { quotes, getRandomQuote } from '@/data/quotesData'
+import ShinyText from '@/components/ShinyText'
+import { useEffect, useState } from 'react'
+
+export default function DevQuotes() {
+  const [quote, setQuote] = useState(() => quotes[0])
+  useEffect(() => {
+    setQuote(getRandomQuote())
+  }, [])
+
+  return (
+    <ShinyText className="text-center text-lg/5 sm:text-lg/5" speed={3} disabled={false}>
+      <blockquote>
+        <p className="break-words">"{quote.text}"</p>
+        <footer>{quote.author}</footer>
+      </blockquote>
+    </ShinyText>
+  )
+}
