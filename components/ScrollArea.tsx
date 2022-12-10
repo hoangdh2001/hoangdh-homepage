fc6e7d21ffe9555422a93ab71c6c5673124268db
Lucas Do <dohuyhoang.se.dev@gmail.com>
2022-12-10 10:00:00 +0900

    feat: add scroll area component

---
+++ components/ScrollArea.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import * as React from 'react'
+import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area'
+
+interface ScrollAreaProps extends React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> {
+  forceScrollbar?: boolean
+}
+
+const ScrollArea = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  ScrollAreaProps
+>(({ className, children, forceScrollbar = false, ...props }, ref) => (
+  <ScrollAreaPrimitive.Root
+    ref={ref}
+    className={`relative overflow-hidden ${className || ''}`}
+    {...props}
+  >
+    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+      {children}
+    </ScrollAreaPrimitive.Viewport>
+    <ScrollBar forceMount={forceScrollbar ? true : undefined} />
+    <ScrollAreaPrimitive.Corner />
+  </ScrollAreaPrimitive.Root>
+))
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
+
+const ScrollBar = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+>(({ className, orientation = 'vertical', ...props }, ref) => (
+  <ScrollAreaPrimitive.ScrollAreaScrollbar
+    ref={ref}
+    orientation={orientation}
+    className={`flex touch-none select-none transition-colors ${
+      orientation === 'vertical'
+        ? 'h-full w-2.5 border-l border-l-transparent p-[1px]'
+        : 'h-2.5 flex-col border-t border-t-transparent p-[1px]'
+    } ${className || ''}`}
+    {...props}
+  >
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-gray-300 dark:bg-gray-600" />
+  </ScrollAreaPrimitive.ScrollAreaScrollbar>
+))
+ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
+
+export { ScrollArea, ScrollBar }
